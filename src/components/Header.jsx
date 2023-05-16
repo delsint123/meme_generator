ea5c23851c16d364a2da7124e6d042b85e21743d
delsint123 <delsint123@gmail.com>
2023-05-16 13:07:59 -0400

    Create and style header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img src="/troll-face.png" className="header--image"/>
+            <h2 className="header--title">Meme Generator</h2>
+            <h3 className="header--subtitle">React Course - Project 3</h3>
+        </header>
+    )
+}
